a9716a41a33eb0c9eeaf50660142b212418bbe49
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2019-10-20 19:30:32 +0200

    validate small_interval test group

--- onlinekval/glasspelet/data/generator.sh
+++ onlinekval/glasspelet/data/generator.sh
@@ -13,11 +13,13 @@ MAXN=200000
 MAXQ=100000
 
 samplegroup
-limits nMax=$MAXN kMax=$MAXN qMax=$MAXQ
+limits nMax=$MAXN kMax=$MAXN qMax=$MAXQ special=none
+sample 1
 
 
 group group1 9
-limits nMax=15 kMax=15 qMax=15
+limits nMax=15 kMax=15 qMax=15 special=none
+tc 1
 tc tiny-01 gen_random n=4 k=2 q=5
 tc tiny-02 gen_random n=11 k=3 q=15
 tc tiny-03 gen_random n=13 k=5 q=15
@@ -32,7 +34,7 @@ tc tiny-11 gen_random n=15 k=5 q=15 mode=smart
 tc tiny-12 gen_random n=15 k=3 q=15 intervals=same
 
 group group2 11
-limits nMax=100 kMax=100 qMax=100
+limits nMax=100 kMax=100 qMax=100 special=none
 include_group group1
 tc small-01 gen_random n=20 k=7 q=45
 tc small-02 gen_random n=30 k=7 q=45
@@ -48,7 +50,7 @@ tc small-11 gen_random n=100 k=10 q=100 mode=smart
 tc small-12 gen_random n=100 k=30 q=100 mode=smart
 
 group group3 17
-limits nMax=1000 kMax=1000 qMax=1000
+limits nMax=1000 kMax=1000 qMax=1000 special=none
 include_group group2
 tc medium-01 gen_random n=300 k=11 q=211
 tc medium-02 gen_random n=600 k=101 q=713
@@ -65,21 +67,21 @@ tc medium-12 gen_random n=1000 k=139 q=1000 mode=smart
 tc medium-13 gen_random n=1000 k=300 q=1000 mode=smart
 
 group group4 5
-limits nMax=$MAXN kMax=1 qMax=$MAXQ
+limits nMax=$MAXN kMax=1 qMax=$MAXQ special=none
 tc k1-01 gen_random n=1 k=1 q=1
 tc k1-02 gen_random n=$MAXN k=1 q=$MAXQ
 tc k1-03 gen_random n=$MAXN k=1 q=$MAXQ intervals=same
 tc k1-04 gen_random n=$((MAXN-1)) k=1 q=$MAXQ intervals=same
 
 group group5 7
-limits nMax=$MAXN kMax=2 qMax=$MAXQ
+limits nMax=$MAXN kMax=2 qMax=$MAXQ special=none
 tc k2-01 gen_random n=2 k=2 q=1
 tc k2-02 gen_random n=13 k=2 q=100
 tc k2-03 gen_random n=$MAXN k=2 q=$MAXQ
 tc k2-04 gen_random n=$MAXN k=2 q=$MAXQ mode=manyzeros x=$((MAXN-4))
 
 group group6 11
-limits nMax=$MAXN kMax=$MAXN qMax=$MAXQ
+limits nMax=$MAXN kMax=$MAXN qMax=$MAXQ special=small_intervals
 tc small_intervals-01 gen_random n=10 k=4 q=13 intervals=small
 tc small_intervals-02 gen_random n=100 k=20 q=100 intervals=small
 tc small_intervals-03 gen_random n=100 k=60 q=100 intervals=small
@@ -91,7 +93,7 @@ tc small_intervals-08 gen_random n=$MAXN k=$((MAXN-1)) q=$MAXQ mode=double inter
 tc small_intervals-09 gen_random n=$MAXN k=3 q=$MAXQ mode=manyzeros intervals=small x=$((MAXN/2))
 
 group group7 23
-limits nMax=$MAXN kMax=$MAXN qMax=1
+limits nMax=$MAXN kMax=$MAXN qMax=1 special=none
 tc q1-01 gen_random n=4000 k=100 q=1 intervals=same
 tc q1-02 gen_random n=7000 k=1001 q=1 intervals=same
 tc q1-03 gen_random n=15000 k=10 q=1
@@ -116,7 +118,7 @@ tc q1-21 gen_random n=$MAXN k=1000 q=1 mode=manyzeros intervals=same x=$((MAXN/2
 tc q1-22 gen_random n=$MAXN k=$((MAXN/10)) q=1 mode=smart intervals=same
 
 group group8 17
-limits nMax=$MAXN kMax=$MAXN qMax=$MAXQ
+limits nMax=$MAXN kMax=$MAXN qMax=$MAXQ special=none
 include_group group3
 include_group group4
 include_group group5
--- onlinekval/glasspelet/input_format_validators/validator/validator.cpp
+++ onlinekval/glasspelet/input_format_validators/validator/validator.cpp
@@ -13,12 +13,17 @@ void run() {
 	vector<int> values = SpacedInts(n, 0, k-1);
 	assert((int)set<int>(values.begin(), values.end()).size() == k);
 
+	string special = Arg("special");
+
 	for(int i = 0; i < q; i++){
 		int l = Int(1,n);
 		Space();
 		int r = Int(1,n);
 		Endl();
 		assert(l <= r);
+		if(special=="small_intervals"){
+			assert(r-l+1 <= k+1);
+		}
 	}
 
 }
