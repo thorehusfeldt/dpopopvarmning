cd7794e47d245c11995d3a3634bc7b1ca43317d5
mattiasutancykeln <55592024+mattiasutancykeln@users.noreply.github.com>
2019-11-17 18:02:33 +0100

    Now generates rooted trees

--- onlinekval/bonsai/data/gen_BIG.py
+++ onlinekval/bonsai/data/gen_BIG.py
@@ -8,7 +8,33 @@ import random
 import math
 import sys
 from solve import solve
+import queue
 
+def find_sol_node(tree):
+
+     steps, bt = [], []
+     pq = queue.PriorityQueue()
+     for ind, line in enumerate(tree):
+         m = len(line)
+         if m == 1:
+             pq.put_nowait((0,ind))
+         bt.append(m)
+         steps.append(0)
+     
+     lst = 0
+     while pq.empty() == 0:
+         _, node = pq.get()
+         lst = node
+         for i in range(len(tree[node])):
+             bt[tree[node][i]] -= 1
+             if steps[tree[node][i]] < steps[node] + 1:
+                 steps[tree[node][i]] = steps[node] + 1
+             else:
+                 steps[tree[node][i]] += 1
+             if bt[tree[node][i]] == 1:
+                 pq.put((steps[tree[node][i]], tree[node][i]))
+     return lst
+ 
 def cmdlinearg(name):
     for arg in sys.argv:
         if arg.startswith(name + "="):
@@ -48,6 +74,7 @@ def main():
     random.seed(int(sys.argv[-1]))
     n = int(cmdlinearg("n"))
     mode = int(cmdlinearg("mode"))
+    rooted = int(cmdlinearg('rooted'))
 
     eds = gen_tree(n, mode)
     lista = [[] for _ in range(n)]
@@ -56,8 +83,14 @@ def main():
         lista[x], lista[y] = str(y), str(x)
         
     string = str(n) + "\n"
-    for l in lista:
-        string += str(len(l)) + " " + ' '.join(l) + "\n"
+    if rooted:
+        lstnode = find_sol_node(lista)
+    
+        for l in lista:
+            string += str(len(l)) + " " + ' '.join(l).replace('0', 'x').replace(str(lstnode), '0').replace('x', str(lstnode)) + "\n"
+    else:
+        for l in lista:
+            string += str(len(l)) + " " + ' '.join(l) + "\n"
     print(string)
 
 if __name__ == "__main__":
