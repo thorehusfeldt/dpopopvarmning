af555ff65f66f01948d00b575d9707a0b4eaf372
Simon Lindholm <simon.lindholm10@gmail.com>
2019-11-27 22:36:36 +0100

    [tvtittande] simplify input validator

--- onlinekval/tvtittande/input_format_validators/validator/validator.cpp
+++ onlinekval/tvtittande/input_format_validators/validator/validator.cpp
@@ -5,17 +5,13 @@
 
 void run() {
 	int nMax = Arg("nMax");
-        int kMax = Arg("kMax");
-	
+	int kMax = Arg("kMax");
+
 	int n = Int(1, nMax);
 	Space();
 	int k = Int(1, kMax);
 	Endl();
-	for(int i = 0; i < k; i++){
-		int len = Int(1, 1000000);
-		if(i != k-1) Space();
-	}
-	Endl();
+	SpacedInts(k, 1, 1000000);
 	long long csum = 0;
 	int prevday = 0;
 	vector<int> w(k);
@@ -26,17 +22,12 @@ void run() {
 		int c = Int(1, 200000);
 		csum += c;
 		Space();
-		set<int> serier;
-		for(int j = 0; j < c; j++){
-			int x = Int(1, k);
-			serier.insert(x);
+		vector<int> serier = SpacedInts(c, 1, k);
+		AssertUnique(serier);
+		for (int x : serier) {
 			w[x-1] = 1;
-			if(j != c-1) Space();
 		}
-		assert(sz(serier) == c);
-		Endl();
 	}
 	assert(csum <= 200000);
 	for(auto x : w) assert(x == 1);
-	Eof();
 }
